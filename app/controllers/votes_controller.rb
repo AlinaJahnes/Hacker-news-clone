f96b631ed96267ea64391ce810b2e7de964ca4b0
Alina Jahnes <alina.jahnes@gmail.com>
2015-08-24 23:39:03 -0400

    add ajax for upvote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,27 +6,18 @@ class VotesController < ApplicationController
   def create
     @article = Article.find_by(id: params[:id])
     @vote = @article.votes.build(vote_params)
-    # if @vote.save && request.xhr?
-    #   render json: {data: @article.votes.count}.to_json
-    # else
+    # if @vote.user_can_vote? && @vote.save && request.xhr?
+    #   render json: {id: @article.id, number: @article.votes.count, status: true}.to_json
+    # elsif request.xhr?
+    #   render json: {notice: "You can only vote once.", status: false}.to_json
     # end
-    if @vote.save && request.xhr?
+    if @vote.user_can_vote? && @vote.save && request.xhr?
       render json: {id: @article.id, number: @article.votes.count}.to_json
     else
-      redirect_to :root, notice: "Fail to vote"
+      flash[:notice] = "You can only vote once per article."
+      redirect_to :root
     end
   end
-
-  # def upvote_count
-  #   @article = Article.find_by(id: params[:id])
-  #   @article.increment!(:like_count)
-  #   @count = @article.like_count
-  #   if request.xhr?
-  #     render json: {number: @count, id: @article.id.to_s }.to_json
-  #   else
-  #   redirect_to :root
-  #   end
-  # end
 
 private
 
