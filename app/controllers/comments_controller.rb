4771b0ad1289909fdda7f18ef20b4e0e0349e2d0
Alina Jahnes <alina.jahnes@gmail.com>
2015-08-25 23:10:42 -0400

    display user and date on each comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,11 @@
 class CommentsController < ApplicationController
   before_action :require_login, only: [:new, :create]
-  # def new
-  #   @comment = Comment.new
-  #   @article = Article.find_by(id: params[:article_id])
-  #   if request.xhr?
-  #     render partial: 'form', layout: false
-  #   end
-  # end
 
   def create
     article = Article.find_by(id: params[:article_id])
     comment = article.comments.build(comment_params)
     if comment.save && request.xhr?
       render json: {data: comment.content}.to_json
-    # if comment.save
-    #   redirect_to article
     else
       redirect_to :root
     end
